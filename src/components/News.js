3dbd29f421f49994e62c8cf0bc14c21061780371
shima1364 <sh.movafagh.474@gmail.com>
2023-01-26 02:38:21 +0100

    5

--- src/components/News.js
+++ src/components/News.js
@@ -5,6 +5,7 @@ import React from 'react';
             <h3>{title}</h3>
             <a target="_blank" href={url}>{url}</a>
             <p>{author}</p>
+            <div class="ui divider"></div>
         </div>
     );
  };
