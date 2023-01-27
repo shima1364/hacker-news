bd231bcc4901a09e4618de071856a5f25a17c2a1
shima1364 <sh.movafagh.474@gmail.com>
2023-01-27 02:17:16 +0100

    1

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Card from './Card';
  const News = ({title , url, author}) => {
     return (
         <div>
-            <h3>{title}</h3>
-            <a target="_blank" href={url}>{url}</a>
-            <p>{author}</p>
+            <Card>
+              <h3>{title}</h3>
+              <a target="_blank" href={url}>{url}</a>
+              <p>{author}</p>
+            </Card>
             <div class="ui divider"></div>
         </div>
     );
